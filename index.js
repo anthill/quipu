5c57f0849194f14c4bec1976558d00ca9400e648
4rzael <maxime.agor@epitech.eu>
2015-08-03 15:22:05 +0200

    a 3G_error added when ssh tunelling fails

--- index.js
+++ index.js
@@ -354,6 +354,7 @@ var dongle = new machina.Fsm({
                             resolve(myProcess);
                         } else if (message.indexOf("Warning: remote port forwarding failed for listen port") !== -1){
                             reject({process: myProcess, msg:"Port already in use."});
+                            self.emit("3G_error");
                         }
                     });
                     // if no error after SSH_TIMEOUT 
